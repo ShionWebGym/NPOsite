75cd0ceca9a8365785747891966c1b4052a652de
Shion Watanabe <shionwatanabe99@gmail.com>
2023-02-21 10:05:58 +0900

    netlify

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `NPOsite`,
-    siteUrl: `https://genuine-arithmetic-39f499.netlify.app/`,
+    title: `NPO小動物疾患研究所`,
+    siteUrl: `https://nposite.netlify.app/`,
     description:`NPO法人小動物疾患研究所の公式サイトです。`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
@@ -11,7 +11,7 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    // "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-netlify-cms",
     "gatsby-plugin-emotion",
     "gatsby-plugin-sass",
   ]
