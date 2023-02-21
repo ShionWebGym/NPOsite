99dc93b89e1fbfe3900bc00e4bf5a0ebeb526469
Shion Watanabe <shionwatanabe99@gmail.com>
2023-02-21 17:47:10 +0900

    a

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -3,66 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import {navbarSubmenuWrapper} from "../components/Dropdown.module.scss";
 
 const Dropdown = () => {
-  const inputElement = useRef(null);
-  console.log(inputElement.current);
-
-  document.addEventListener("DOMContentLoaded", () => {
-    DropdownMenu.init();
-  });
-
-  /**
-   * ドロップダウンメニュー
-   * constructor
-   * @param {HTMLElement} elem 開くための要素
-   * @property {HTMLElement} button 開くための要素
-   * @property {HTMLElement} target 開閉される要素
-   * @property {number} contentHeight サブメニューの高さ
-   */
-  function DropdownMenu(this: any, elem: Element) {
-    this.button = elem;
-
-    const targetId = this.button.dataset.menu;
-    this.target = document.getElementById(targetId);
-
-    const content = this.target.children[0];
-    this.contentHeight = content.clientHeight;
-
-    this.handleEvent();
-  }
-
-  /**
-   * イベント登録
-   */
-  DropdownMenu.prototype.handleEvent = function () {
-    // マウスオーバーで開く
-    this.button.addEventListener("mouseover", this.open.bind(this));
-
-    // マウスアウトで閉じる
-    this.button.addEventListener("mouseout", this.close.bind(this));
-    
-  };
-
-  /**
-   * 開く
-   */
-  DropdownMenu.prototype.open = function () {
-    this.target.style.height = this.contentHeight + "px";
-  };
-
-  /**
-   * 閉じる
-   */
-  DropdownMenu.prototype.close = function () {
-    this.target.style.height = "0";
-  };
-
-  /**
-   * 初期化
-   */
-  DropdownMenu.init = function () {
-    const menus = document.querySelectorAll("[data-menu]");
-    // menus.forEach((menu) => new DropdownMenu(menu));
-  };
+  
   return (
     <div>
       <ul>
@@ -79,7 +20,6 @@ const Dropdown = () => {
         <li data-menu="menu-3"></li>
       </ul>
       <div />
-      <input ref={inputElement.current} type="text" />
       <ul>
         <li>listitems1</li>
         <li>listitems2</li>
