3115b84c8819f773741c2cdec01be73d98e287dc
Shion Watanabe <shionwatanabe99@gmail.com>
2023-05-16 17:59:19 +0900

    topimage fix

--- src/components/TopImage.tsx
+++ src/components/TopImage.tsx
@@ -10,24 +10,24 @@ const TopImage = () => {
     <div className={topimage}>
       <div className={top_text_box}>
         <motion.h1
-          initial={{ opacity: 0, y: -100 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 3 }}
+          initial={{ opacity: 0, x:40 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 2 }}
         >
           人と伴侶動物をめぐる<br />数々の問題の解消
           <br />
         </motion.h1>
         <motion.h2
-          initial={{ opacity: 0, y: -100 }}
-          animate={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, x: -40 }}
+          animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 2 }}
         >
           ・意識向上を図る指導と教育の広報
         </motion.h2>
         <motion.h2
-          initial={{ opacity: 0, y: -100 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1}}
+          initial={{ opacity: 0, x: 40 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 2}}
         >
           ・医療従事者の知識の向上を目指します。
         </motion.h2>
